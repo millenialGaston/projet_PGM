19842d48d19da0381c9a691c73daafca43a74b29
jimleroux <35265181+jimleroux@users.noreply.github.com>
2018-11-22 11:42:31 -0500

    Create hmmjokegen.py
    Python folder for the codes

---
+++ Python/hmmjokegen.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+Project for IFT6269.
+"""
+
+__authors__ = "Jimmy Leroux, Nicolas Laliberte, Frederic Boileau"
+__version__ = "1.0"
+__maintainer__ = "Jimmy Leroux, Nicolas Laliberte, Frederic Boileau"
+__email__ = "jim.leroux1@gmail.com, n.laliberte01@gmail.com, "
+__studentid__ = "1024610, 1005803, "
+
+import numpy
+
+if __name__ == '__main__':
+	print('WAZAAA')
