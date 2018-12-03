f1fb56ff8ade06cfcefe59d283bc996fdbbfce3f
jimleroux <jim.leroux1@gmail.com>
2018-12-02 22:55:19 -0500

    Update script.py

--- python/textGenerator/script.py
+++ python/textGenerator/script.py
@@ -54,7 +54,7 @@ def main(*args,**kwargs):
     target_vocab = list(set(dataset))
     t_vocab = {k:v for v,k in enumerate(target_vocab)}
 
-    rnn = textGenerator.RNN(device, len(target_vocab),256).to(device)
+    rnn = textGenerator.RNN(device, len(target_vocab),256, len(target_vocab)).to(device)
 
     loss_train, loss_test = textGenerator.train(rnn, device,dataset,t_vocab, num_epoch=20,
                                                 sequence_size=100, batch_size=64, lr=0.01)
