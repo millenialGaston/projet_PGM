e6aa23f96cd6b942c9dee7655af89da0ec7bc5b9
jimleroux <jim.leroux1@gmail.com>
2018-12-06 10:17:56 -0500

    Update textGenerator.py

--- python/textGenerator/textGenerator.py
+++ python/textGenerator/textGenerator.py
@@ -363,7 +363,7 @@ def train(model, device, dataset, t_vocab, target_vocab, num_epoch=20,
             # Learning rate decay.
             #lrd = lr * (1./(1 + 4 * epoch / num_epoch))
             # Define the optimizing method and pass the parameters to optimize.
-            optimizer = torch.optim.Adam(model.parameters(), lr=lrd)
+            optimizer = torch.optim.Adam(model.parameters(), lr=lr)
             # zero the gradient after each step.
             model.zero_grad()
             # get a training exemple.
