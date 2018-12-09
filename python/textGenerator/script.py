b7742b6e73762ea436126bd277292c4e82a8a6d5
jimleroux <jim.leroux1@gmail.com>
2018-12-09 17:12:49 -0500

    Update script.py

--- python/textGenerator/script.py
+++ python/textGenerator/script.py
@@ -81,6 +81,7 @@ def main(*args,**kwargs):
   saveModels(models)
   torch.save(classifier.state_dict(),'C:/Users/Jimmy/Desktop/' + 'classifier')
 
+  return (d,l) 
 def fetchData(name : str, extension : str, filtering=False) -> str:
   dataPath = 'data/'
   dataset = None
@@ -220,4 +221,4 @@ def cliParsing():
   args = parser.parse_args()
 
 if __name__ == '__main__':
-  main()
+  d,l = main()
